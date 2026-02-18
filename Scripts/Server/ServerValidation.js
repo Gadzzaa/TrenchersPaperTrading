202049e1f1c8809761f273288b7de1e707fa9f46
Gadzzaa <popescurobert986@gmail.com>
2026-02-18 04:07:54 +0200

    fix: ServerValidation
    - would return version = undefined.
    - Version > Static Variable

--- Scripts/Server/ServerValidation.js
+++ Scripts/Server/ServerValidation.js
@@ -2,14 +2,14 @@ import { AppError } from "../ErrorHandling/Helpers/AppError.js";
 import { BackendRequest } from "./BackendRequest.js";
 
 export class ServerValidation {
-  version = this.chrome.runtime.getManifest().version;
+  static #version = chrome.runtime.getManifest().version;
 
   /**
    *  @returns {Promise<boolean>} - true if the current version is the latest, false otherwise
    * */
   static async isLatestVersion() {
     const response = await new BackendRequest()
-      .addEndpoint("/latest?version=" + this.version)
+      .addEndpoint("/latest?version=" + this.#version)
       .addMethod("GET")
       .addRetries(2)
       .build();
