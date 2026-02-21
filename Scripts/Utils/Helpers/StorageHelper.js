226a93d0e502e8be9d3cfbe103a6d325be31a3df
Gadzzaa <popescurobert986@gmail.com>
2026-02-21 19:46:24 +0200

    refactor: UIHelper

--- Scripts/Utils/Helpers/StorageHelper.js
+++ Scripts/Utils/Helpers/StorageHelper.js
@@ -1,32 +1,5 @@
 export class StorageHelper {
   /**
-   * @param {HTMLButtonElement} button - Button element to show loading dots on
-   * @returns {number} - Interval ID to clear later
-   */
-  static startLoadingDots(button) {
-    let dots = 0;
-    button.dataset.originalText = button.textContent; // save original text
-    document.body.style.pointerEvents = "none";
-
-    const interval = setInterval(() => {
-      dots = (dots + 1) % 4; // 0 → 1 → 2 → 3 → 0
-      button.textContent = ".".repeat(dots);
-    }, 250);
-
-    return interval; // return interval ID so you can clear it later
-  }
-
-  /**
-   * @param {HTMLButtonElement} button - Button element to stop loading dots on
-   * @param {number} interval - Interval ID returned by startLoadingDots
-   */
-  static stopLoadingDots(button, interval) {
-    clearInterval(interval);
-    button.textContent = button.dataset.originalText;
-    document.body.style.pointerEvents = "auto";
-  }
-
-  /**
    * @param {string} key - Key to retrieve from storage
    * @returns {Promise<string>} - Key value from storage
    */
