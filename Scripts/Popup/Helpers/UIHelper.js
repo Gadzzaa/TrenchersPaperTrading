d62fb7acfc29bf4ea55e58643483d2dbcaa0c011
Gadzzaa <popescurobert986@gmail.com>
2026-02-22 16:16:08 +0200

    fix: make setQualityPreset static refactor: clearInputFields func

--- Scripts/Popup/Helpers/UIHelper.js
+++ Scripts/Popup/Helpers/UIHelper.js
@@ -1,23 +1,33 @@
 export class UIHelper {
-  setQualityPreset(qualityValue) {
-    document.querySelector(".pageCarousel").style.scrollBehavior =
-      qualityValue < 3 ? "auto" : "smooth";
+    static setQualityPreset(qualityValue) {
+        document.querySelector(".pageCarousel").style.scrollBehavior =
+            qualityValue < 3 ? "auto" : "smooth";
 
-    const buttons = document.querySelectorAll("button");
-    if (qualityValue < 2) {
-      buttons.forEach((btn) => btn.style.removeProperty("will-change"));
-    } else {
-      buttons.forEach(
-        (btn) =>
-          (btn.style.willChange =
-            "transform, background-color, color, box-shadow, border-color"),
-      );
+        const buttons = document.querySelectorAll("button");
+        if (qualityValue < 2) {
+            buttons.forEach((btn) => btn.style.removeProperty("will-change"));
+        } else {
+            buttons.forEach(
+                (btn) =>
+                    (btn.style.willChange =
+                        "transform, background-color, color, box-shadow, border-color"),
+            );
+        }
+
+        if (qualityValue < 1) {
+            buttons.forEach((btn) => btn.classList.add("no-shadow"));
+        } else {
+            buttons.forEach((btn) => btn.classList.remove("no-shadow"));
+        }
     }
 
-    if (qualityValue < 1) {
-      buttons.forEach((btn) => btn.classList.add("no-shadow"));
-    } else {
-      buttons.forEach((btn) => btn.classList.remove("no-shadow"));
+    static clearInputFields() {
+        let usernameInput = document.getElementById("formUsername")
+        let passwordInput = document.getElementById("formPassword");
+
+        if (usernameInput) usernameInput.value = "";
+        if (passwordInput) passwordInput.value = "";
     }
-  }
+
+   
 }
