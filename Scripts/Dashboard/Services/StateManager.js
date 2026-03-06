705b178c4f8b12bb8d29f11afe18f21d6bf8ad74
Gadzzaa <popescurobert986@gmail.com>
2026-03-06 16:19:05 +0200

    refactor: cleanup
    fix: InitHelper.loadSettings call

--- Scripts/Dashboard/Services/StateManager.js
+++ Scripts/Dashboard/Services/StateManager.js
@@ -1,66 +1,74 @@
-import { InitHelper } from "../Helpers/InitHelper.js";
-import { UIHelper } from "../Helpers/UIHelper.js";
-import { startInterval } from "../Helpers/IntervalHelper.js";
-import { UIManager } from "../../Utils/Core/UIManager.js";
-import { PNLService } from "./PNLService.js";
+import {InitHelper} from "../../Utils/Helpers/InitHelper.js";
+import {UIHelper} from "../Helpers/UIHelper.js";
+import {startInterval} from "../Helpers/IntervalHelper.js";
+import {UIManager} from "../../Utils/Core/UIManager.js";
+import {PNLService} from "./PNLService.js";
+import {UIConfig} from "../Config/UIConfig.js"
+import {ErrorHandler} from "../../ErrorHandling/Core/ErrorHandler.js";
 
 export class StateManager {
-  constructor() {
-    this.initializing = false;
-    this.healthy = false;
-    this.dataUpdater = null;
+    constructor() {
+        this.initializing = false;
+        this.running = false;
+        this.healthy = false;
 
-    this.variables = null;
-    this.dataManager = null;
-    this.pnlService = new PNLService();
+        this.variables = null;
+        this.pnlService = new PNLService(this);
 
-    this.updateInterval = null;
+        this.updateInterval = null;
 
-    this.fetchingBalance = false;
-    this.currentPreset = null;
-    this.currentContract = null;
-  }
+        this.fetchingBalance = false;
+        this.currentPreset = null;
+        this.currentContract = null;
+    }
 
-  async initialize() {
-    console.log("[TrenchersPT] 🟢 Initializing dashboard...");
-    if (this.initializing) return;
-    this.initializing = true;
+    async initialize() {
+        try {
+            if (this.initializing || this.running) return;
+            console.log("[TrenchersPT] 🟢 Initializing dashboard...");
+            this.initializing = true;
 
-    InitHelper.loadSettings();
+            InitHelper.loadSettings(UIConfig);
 
-    await InitHelper.validateHealth(this);
+            await InitHelper.validateHealth(this);
 
-    await InitHelper.validateVersion(this);
+            await InitHelper.validateVersion(this);
 
-    await InitHelper.validateSession(this);
-    await this.pnlService.start();
+            await InitHelper.validateSession(this);
+            await this.pnlService.start();
 
-    UIHelper.clearUI();
+            UIHelper.clearUI();
 
-    document.body.style.removeProperty("pointer-events");
+            document.body.style.removeProperty("pointer-events");
 
-    clearInterval(this.updateInterval);
-    this.updateInterval = startInterval(this);
+            this.updateInterval = startInterval(this);
 
-    this.initializing = false;
-    UIManager.enableUI();
-  }
+            await UIManager.enableUI();
+            this.initializing = false;
+            this.running = true;
+        } catch (err) {
+            this.initializing = false;
+            throw ErrorHandler.log(err);
+        }
+    }
 
-  async disconnect() {
-    console.log("[TrenchersPT] 🔴 Disconnecting dashboard...");
+    async disconnect() {
+        console.log("[TrenchersPT] 🔴 Disconnecting dashboard...");
 
-    document.body.style.pointerEvents = "none";
+        document.body.style.pointerEvents = "none";
 
-    clearInterval(this.updateInterval);
-    this.updateInterval = null;
+        clearInterval(this.updateInterval);
+        this.updateInterval = null;
 
-    this.currentContract = null;
+        this.currentContract = null;
 
-    this.pnlService.stop();
-  }
+        this.pnlService.stop();
 
-  async logout() {
-    this.disconnect();
-    await UIManager.disableUI("no-session");
-  }
+        this.running = false;
+    }
+
+    async logout() {
+        await this.disconnect();
+        await UIManager.disableUI("no-session");
+    }
 }
