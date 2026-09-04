a7f93d965b76e9303307f25b7e7dd4b8cc4b907a
Gadzzaa <popescurobert986@gmail.com>
2026-09-05 02:05:14 +0300

    refactor: StorageHelper's Logic has been moved to StorageManager

--- Scripts/Utils/Core/StorageManager.js
+++ Scripts/Utils/Core/StorageManager.js
@@ -1,40 +1,42 @@
-import { StorageHelper } from "../Helpers/StorageHelper.js";
-import { AppError } from "../../ErrorHandling/Helpers/AppError.js";
+import {AppError} from "../../ErrorHandling/Helpers/AppError.js";
 
 export class StorageManager {
-  static async getFromStorage(key) {
-    try {
-      return await StorageHelper.getFromStorage(key);
-    } catch (err) {
-      throw new AppError("Failed to get data from storage", {
-        code: "STORAGE_GET_FAILED",
-        cause: err,
-        meta: { key },
-      });
+    static async getFromStorage(key) {
+        try {
+            const result = await chrome.storage.local.get(key);
+            return result[key];
+        } catch (err) {
+            throw new AppError("Failed to get data from storage", {
+                code: "STORAGE_GET_FAILED",
+                cause: err,
+                meta: {key},
+            });
+        }
     }
-  }
 
-  static async setToStorage(key, value) {
-    try {
-      await StorageHelper.setToStorage(key, value);
-    } catch (err) {
-      throw new AppError("Failed to set data to storage", {
-        code: "STORAGE_SET_FAILED",
-        cause: err,
-        meta: { key, value },
-      });
+    static async setToStorage(key, value) {
+        try {
+            await chrome.storage.local.set({
+                [key]: value,
+            });
+        } catch (err) {
+            throw new AppError("Failed to set data to storage", {
+                code: "STORAGE_SET_FAILED",
+                cause: err,
+                meta: {key, value},
+            });
+        }
     }
-  }
 
-  static async removeFromStorage(key) {
-    try {
-      await StorageHelper.removeFromStorage(key);
-    } catch (err) {
-      throw new AppError("Failed to remove data from storage", {
-        code: "STORAGE_REMOVE_FAILED",
-        cause: err,
-        meta: { key },
-      });
+    static async removeFromStorage(key) {
+        try {
+            await chrome.storage.local.remove(key);
+        } catch (err) {
+            throw new AppError("Failed to remove data from storage", {
+                code: "STORAGE_REMOVE_FAILED",
+                cause: err,
+                meta: {key},
+            });
+        }
     }
-  }
 }
