cd50604867ecaa4446163baaa2df2a21c7824ae8
Gadzzaa <popescurobert986@gmail.com>
2026-09-05 03:29:13 +0300

    refactor(AccountUILogic): Remove redundant async/await pairs & implement proper try/catch

--- Scripts/Popup/Helpers/AccountUILogic.js
+++ Scripts/Popup/Helpers/AccountUILogic.js
@@ -7,46 +7,51 @@ import {AccountLoader} from "../Core/AccountLoader.js";
 
 export class AccountUILogic {
     static async resetAccount(stateManager) {
-        let amount = await DialogsValidators.askStartupBalance(stateManager);
+        const amount = await DialogsValidators.askStartupBalance(stateManager);
         if (!amount) return;
 
-        let confirmed = await DialogsValidators.askResetConfirmation(stateManager);
+        const confirmed = await DialogsValidators.askResetConfirmation(stateManager);
         if (!confirmed) return;
 
         let dataManager = new DataManager(stateManager);
 
-        await dataManager
-            .resetAccount(amount)
-            .then(() => {
-                FooterHelper.focusDefaultButton();
-                AccountLoader.loadData(stateManager);
-            })
-            .catch((err) => {
-                throw ErrorHandler.log(err);
-            });
-    }
-
-
-    static async upgradeSubscription(plan, stateManager) {
-        let subscriptionManager = new SubscriptionManager(stateManager);
-        await subscriptionManager
-            .upgradeSubscription(plan)
-            .catch((err) => {
-                throw ErrorHandler.log(err);
-            })
-            .finally(() => {
-                AccountUILogic.hideSubscriptionDiv();
-            });
-    }
-
-    static async manageSubscription(stateManager) {
-        let subscriptionManager = new SubscriptionManager(stateManager);
-        await subscriptionManager.manageSubscription().catch((err) => {
+        try {
+            await dataManager.resetAccount(amount);
+            FooterHelper.focusDefaultButton();
+            await AccountLoader.loadData(stateManager);
+        } catch (err) {
             throw ErrorHandler.log(err);
-        }).finally
-        (() => {
+        }
+    }
+
+    static upgradeSubscription(plan, stateManager) {
+        const subscriptionManager = new SubscriptionManager(stateManager);
+
+        return AccountUILogic.#runSubscriptionOperation(
+            () => subscriptionManager.upgradeSubscription(plan),
+        );
+    }
+
+    static manageSubscription(stateManager) {
+        const subscriptionManager = new SubscriptionManager(stateManager);
+
+        return AccountUILogic.#runSubscriptionOperation(
+            () => subscriptionManager.manageSubscription(),
+        );
+    }
+
+    /**
+     * @param {() => Promise<void>} operation
+     * @returns {Promise<void>}
+     */
+    static async #runSubscriptionOperation(operation) {
+        try {
+            await operation();
+        } catch (error) {
+            throw ErrorHandler.log(error);
+        } finally {
             AccountUILogic.hideSubscriptionDiv();
-        });
+        }
     }
 
     static showSubscriptionDiv() {
